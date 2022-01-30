984da41ac455552a200aa8f999d920df6efed9f0
Joselito Vam's <vamsjose@gmail>
2022-01-30 14:18:26 +0100

    modification de deux fichier

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('test', [Controller::class], 'index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
